990a206cdfa7dea9873c56ffda0d094d441dd8f1
Contra <contra@maricopa.edu>
2015-02-22 04:29:18 -0800

    create react style insertion mixin which lazily inserts css as components are loaded, fix tests using render to static markup

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -2,11 +2,14 @@
 
 var React = require('react');
 var merge = require('lodash.merge');
-//var styles = require('./index.styl');
+var StyleMixin = require('../../lib/StyleMixin');
+var css = require('./index.styl');
 var DOM = React.DOM;
 
 module.exports = React.createClass({
   displayName: 'Modal',
+  mixins: [StyleMixin],
+  css: css,
 
   render: function(){
     var props = merge({
--- test/components/Modal.js
+++ test/components/Modal.js
@@ -7,7 +7,7 @@ var Modal = React.createFactory(require('../../components/Modal'));
 describe('Modal()', function() {
   it('should render with children', function(done){
     var modal = Modal(null, 'Test');
-    var str = React.renderToString(modal);
+    var str = React.renderToStaticMarkup(modal);
     should.exist(str);
     str.should.equal('<div class="modal-component">Test</div>');
     done();

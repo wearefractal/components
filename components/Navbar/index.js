126f96b2208880b1997df19ab1c110172bd40b66
Steve Lacy <me@slacy.me>
2015-02-23 23:52:18 -0800

    add mobile nav

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -20,12 +20,19 @@ module.exports = React.createClass({
     title: PropTypes.string
   },
 
+  getInitialState: function() {
+    return {
+      toggleNav: false
+    };
+  },
   getDefaultProps: function(){
     return {
       links: []
     };
   },
-
+  toggleNav: function() {
+    this.setState({toggleNav: !this.state.toggleNav});
+  },
   render: function(){
     var props = merge({
       className: 'navbar-component'
@@ -35,15 +42,29 @@ module.exports = React.createClass({
       className: 'title'
     }, this.props.title);
 
+    var button = DOM.div({
+      className: 'navbar-button',
+      onClick: this.toggleNav
+    },
+      DOM.div({className: 'bar'}),
+      DOM.div({className: 'bar'}),
+      DOM.div({className: 'bar'})
+    );
+
+    var self = this;
     var links = this.props.links.map(function(link) {
       link.key = link.to + '-nav-link';
+      link.onClick = self.toggleNav;
       return Link(link, link.label);
     });
 
     var inner = DOM.div({
-      className: 'inner'
+      className: 'inner',
+      style: {
+        maxHeight: this.state.toggleNav ? '600' : '0'
+      }
     }, links);
 
-    return DOM.nav(props, title, inner);
+    return DOM.nav(props, title, inner, button);
   }
 });
